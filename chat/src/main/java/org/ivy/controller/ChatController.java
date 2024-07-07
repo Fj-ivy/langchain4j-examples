0bc31688b8ff6e9487659400edf8ac398c054c1b
fangjie33 <fangjie33@jd.com>
2024-07-07 23:17:37 +0800

    修改入门代码

--- chat/src/main/java/org/ivy/controller/ChatController.java
+++ chat/src/main/java/org/ivy/controller/ChatController.java
@@ -1,6 +1,6 @@
 package org.ivy.controller;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
-    private final ChatLanguageModel chatLanguageModel;
+    private final OllamaChatModel ollamaChatModel;
 
-    public ChatController(ChatLanguageModel chatLanguageModel) {
-        this.chatLanguageModel = chatLanguageModel;
+    public ChatController(OllamaChatModel ollamaChatModel) {
+        this.ollamaChatModel = ollamaChatModel;
     }
 
     @GetMapping("chat")
     public String chat(String prompt) {
-        return chatLanguageModel.generate(prompt);
+        return ollamaChatModel.generate(prompt);
     }
 }
